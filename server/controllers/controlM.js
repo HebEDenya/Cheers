054ab7fb848cd428c9ccee6afd068b01c61a7505
ouichniaziz <medazizouichni@gmail.com>
2021-07-16 15:03:14 +0100

    server side

--- server/controllers/controlM.js
+++ server/controllers/controlM.js
@@ -1,9 +1,11 @@
-const {handle} = require('../queries/query_user/queryM.js')
+const { handle } = require("../queries/query_user/queryM.js");
 
 const selectRequest = (req, res) => {
-    handle().then((result)=> { res.send(result)})
-}
+  handle().then((result) => {
+    res.send(result);
+  });
+};
 
 module.exports = {
-    selectRequest,
-}
+  selectRequest,
+};

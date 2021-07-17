4325f59226f4211ae07219c0b6df4d959136a39d
marwaZhr <zahar.marwa.13@gmail.com>
2021-07-17 23:56:12 +0200

    create event front end v2

--- src/components/CreateEvent.tsx
+++ src/components/CreateEvent.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import {IonToolbar, IonLabel , IonPage, IonHeader, IonTitle,IonText,IonContent, IonInput,IonSelectOption, IonItem, IonList, IonSegment, IonIcon,IonSegmentButton, IonTextarea,IonListHeader, IonSelect } from '@ionic/react';
-import { locate, wifi } from 'ionicons/icons';
+import {IonToolbar, IonLabel , IonPage, IonHeader, IonTitle,IonText,IonContent, IonInput,IonSelectOption, IonItem, IonList, IonSegment, IonIcon,IonSegmentButton, IonTextarea,IonListHeader, IonSelect, IonDatetime, IonButton } from '@ionic/react';
+import { locate, wifi,card, star } from 'ionicons/icons';
 import './CreateEvent.scss'
 
 interface ContainerProps {
@@ -13,6 +13,11 @@ const CreateEventComponenet: React.FC<ContainerProps> = () => {
   const [categorie, setCategorie] = useState<string>('');
   const [location, setLocation] = useState<string>('');
   const [adress, setAdress] = useState<string | null>(null)
+  const [selectedStartDate, setSelectedStartDate] = useState<string>('');
+  const[ selectEndDate,setSelectEndDate]= useState<string>('');
+  const[ selectPrice,setSelectPrice]= useState<string>('');
+  const [quantity, setQuantity] = useState<number | null>(null);
+
 
 
 
@@ -25,23 +30,24 @@ const CreateEventComponenet: React.FC<ContainerProps> = () => {
         </IonToolbar>
       </IonHeader>
       {/* {For the basic info} */}
+      &nbsp;
       <IonContent>
        <IonList>
        <IonListHeader>
-            <IonLabel color="">
+            <IonLabel className="color_title_create">
             Basic Info
         </IonLabel>
         </IonListHeader>
         <IonItem lines="none">
-        <IonText> Add details that highlight what makes it unique.</IonText>
+        <IonText className="color_subtitle_create" > Add details that highlight what makes it unique.</IonText>
         </IonItem>
-      <IonItem>
+      <IonItem className="input_create_Event">
         <IonLabel position="floating">Title</IonLabel>
         <IonInput type="text" name="title" value={title} onIonChange={e => setTitle(e.detail.value!)} 
-        clearInput required spellcheck  maxlength= {50}> 
+        clearInput required spellcheck  maxlength= {50} > 
         </IonInput>
         </IonItem>
-        <IonItem>
+        <IonItem className="input_create_Event">
             <IonLabel position="floating">Description</IonLabel>
             <IonTextarea name="description" value={description} onIonChange={e => {setDescription(e.detail.value!); 
             }} clearOnEdit required spellcheck autoGrow maxlength= {2000}></IonTextarea>
@@ -49,7 +55,7 @@ const CreateEventComponenet: React.FC<ContainerProps> = () => {
        </IonList>  
        &nbsp;
 
-       <IonItem>
+       <IonItem className="input_create_Event">
         &nbsp;
             <IonLabel>Categories </IonLabel>
             <IonSelect value={categorie} okText="Okay" cancelText="Dismiss" onIonChange={e => {setCategorie(e.detail.value); console.log(categorie);
@@ -66,16 +72,16 @@ const CreateEventComponenet: React.FC<ContainerProps> = () => {
       {/* {For the localisation} */}
           <IonList>
        <IonListHeader>
-          <IonLabel color="">
+          <IonLabel className="color_title_create">
             Localisation
         </IonLabel>
         </IonListHeader>
         <IonItem lines="none">
-        <IonText> Help people in the area discover your event and let attendees know where to show up.</IonText>
+        <IonText className="color_subtitle_create"> Help people in the area discover your event and let attendees know where to show up.</IonText>
         </IonItem>
         </IonList>
         &nbsp;
-        <IonSegment onIonChange={e => {setLocation( e.detail.value);}} color="primary" className="location_height_create">
+        <IonSegment onIonChange={e => {setLocation( e.detail.value);}} color="primary" className="location_height_create " >
           <IonSegmentButton value="venue" >
             <IonLabel>Venue</IonLabel><IonIcon icon={locate} />
           </IonSegmentButton>
@@ -84,16 +90,86 @@ const CreateEventComponenet: React.FC<ContainerProps> = () => {
           </IonSegmentButton>
         </IonSegment>
         &nbsp;
-        {location === "venue" ? <IonItem>
+        {location === "venue" ? <IonItem className="input_create_Event">
         <IonLabel position="floating">Adress of the event </IonLabel>
-        <IonInput type="text" name="adress" value={adress} onIonChange={e => setAdress(e.detail.value!)} 
+        <IonInput type="text" name="adress" value={adress} onIonChange={e => setAdress(e.detail.value!) } 
         clearInput required > 
         </IonInput>
         </IonItem> : ""}
-      </IonContent>
+        &nbsp;
+      {/* {For the Date and time} */}
+        <IonList>
+       <IonListHeader>
+            <IonLabel className="color_title_create">
+            Date and Time
+        </IonLabel>
+        </IonListHeader>
+        <IonItem lines="none">
+        <IonText className="color_subtitle_create" > Tell event-goers when your event starts and ends so they can make plans to attend.</IonText>
+        </IonItem>     
+       </IonList>  
+       &nbsp;
+       {/* {Start Time and sate } */}
+        <IonItem lines="none" className="input_create_Event">
+          <IonLabel>Event Start</IonLabel>
+          <IonDatetime displayFormat=" MMM D, YYYY"  max="2099" min="2021" value={selectedStartDate} onIonChange={e => setSelectedStartDate(e.detail.value!)}></IonDatetime>
+        </IonItem>
+        <IonItem className="input_create_Event" >
+          <IonLabel>Start time</IonLabel>
+          <IonDatetime displayFormat="h:mm a" value={selectedStartDate} onIonChange={e => setSelectedStartDate(e.detail.value!)}></IonDatetime>
+        </IonItem>
+
+        {/* {end Time and date of event } */}
+        <IonItem lines="none" className="input_create_Event">
+          <IonLabel>Event End</IonLabel>
+          <IonDatetime displayFormat=" MMM D, YYYY"  max="2099" min="2021" value={selectEndDate} onIonChange={e => setSelectEndDate(e.detail.value!)}></IonDatetime>
+        </IonItem>
+        <IonItem className="input_create_Event" lines="none">
+          <IonLabel>End time</IonLabel>
+          <IonDatetime displayFormat="h:mm a" value={selectEndDate} onIonChange={e => setSelectEndDate(e.detail.value!)}></IonDatetime>
+        </IonItem>
+       &nbsp;
+       {/* {Price and Free } */}
+       <IonList>
+       <IonListHeader>
+            <IonLabel className="color_title_create">
+            Date and Time
+        </IonLabel>
+        </IonListHeader>
+        <IonItem lines="none">
+        <IonText className="color_subtitle_create" > Tell event-goers the price of your event.</IonText>
+        </IonItem>     
+       </IonList> 
+       &nbsp;
+        <IonSegment onIonChange={e => {setSelectPrice( e.detail.value);}} color="primary" className="location_height_create " >
+          <IonSegmentButton value="free" >
+            <IonLabel>Free</IonLabel><IonIcon icon={star} />
+          </IonSegmentButton>
+          <IonSegmentButton value="paied">
+            <IonLabel>Paid</IonLabel><IonIcon icon={card} />
+          </IonSegmentButton>
+        </IonSegment>
+        &nbsp;
+        <IonItem className="input_create_Event">
+        <IonLabel position="floating"> Available places or quantities  </IonLabel>
+        <IonInput  name="quantity" value={quantity} onIonChange={e => setQuantity(+e.detail.value!) } 
+        clearInput required > 
+        </IonInput>
+        </IonItem>
+        &nbsp;
+        <IonItem lines="none"  >
+        &nbsp;
+        <IonButton size="default"  className="button_create_event">Cancel</IonButton>
+        <IonButton size="default"  className="button_create_event">Confirme</IonButton>
+        </IonItem>
+        &nbsp;
+        
+      </IonContent>     
     </IonPage>
     </>
   );
 };
 
 export default CreateEventComponenet;
+
+ 
